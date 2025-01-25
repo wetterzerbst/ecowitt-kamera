0c0d13058903ea41ec7e56c06f1c9a976f2995d4
Wetterstation Zerbst-Dorfstätte <wetter.zerbst@gmail.com>
2025-01-25 03:46:12 +0100

    Create webcam.php
    Einbindung des erzeugten Bildes bild.php in eine Webseite, mit der Option, das dieses Bild tatsächlich alle 30 Sekunden neu geladen wird, um eventuelle Caching-Probleme zu umgehen.

---
+++ webcam.php
@@ -0,0 +1,5 @@
+<?php clearstatcache(); ?><meta http-equiv="refresh" content="30">
+<title>Webcam</title>
+<b>Kamera</b> <small><small>+ + + LIVE + + +</small></small><br><br>
+<img src="bild.php?filetime=<?php echo(date("Y-m-d_H:i:s")); ?>" width="50%" height="50%" />
+<br>
